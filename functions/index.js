18528a189fe00a19402ccad3da42a2bad55a4766
Vincent Tam <vtam2012@gmail.com>
2019-01-01 00:32:56 -0800

    added new cloud function to notify user of project deletion

--- functions/index.js
+++ functions/index.js
@@ -27,6 +27,19 @@ exports.projectCreated = functions.firestore
     return createNotification(notification);
 })
 
+exports.projectDeleted = functions.firestore
+.document('projects/{projectId}')
+.onDelete(doc => {
+    const project = doc.data();
+    const notification = {
+        content: `Deleted project: ${project.title}`,
+        user: `${project.authorFirstName} ${project.authorLastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+    }
+    
+    return createNotification(notification);
+})
+
 exports.userJoined = functions.auth.user()
     .onCreate(user => {
         return admin.firestore().collection('users')
